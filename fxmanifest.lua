6e153ae9a61c752d5adb710dffc4a16bb70f19bf
AMP <achrafhoufaty@gmail.com>
2026-09-06 23:34:29 +0200

    0.0.11

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'qbx_properties'
 description 'Hopefully one day a feature rich property system'
 repository 'https://github.com/Qbox-project/qbx_properties'
-version '0.0.10'
+version '0.0.11'
 
 ox_lib 'locale'
 
